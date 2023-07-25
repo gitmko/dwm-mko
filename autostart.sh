436186497856a3509cbe35f10fc83e93923a0aa3
mko <emkayohh@proton.me>
2023-07-25 14:43:41 +0200

    Some fixes
    Finally actually fixing the freaking fonts script.
    Adding gnome-polkit to install script and autostart.sh.

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,16 @@
+oscheck="$(lsb_release -is)"
+
+if [[ $oscheck == "Arch" ]] 
+then
+	bash /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+elif [[ $oscheck == "Debian" ]]
+then
+    bash /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1
+# elif [[ $oscheck == "Fedora" ]]
+# then
+        # /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 (or sum like this)
+fi
+
 bash $HOME/.dwm/scripts/bar &
 bash $HOME/.fehbg &
 volumeicon &
